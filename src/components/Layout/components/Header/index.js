f1bb022954394561f7761c44d547865c8ff0c6de
ChungNV <nguyenvanchung281296@gmail.com>
2023-03-30 22:33:27 +0700

    update css header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,15 @@
+import styles from './Header.module.scss';
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>
+    return <h2 className={cx('wrapper')}>
+        <div className={cx('inner')}>
+        {/*    Logo */}
+        {/*    Search */}
+        </div>
+    </h2>;
 }
 
 export default Header;
