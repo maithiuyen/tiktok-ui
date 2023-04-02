b7c6f131119e8dd36b586260c27c89e6dc1552e1
ChungNV <nguyenvanchung281296@gmail.com>
2023-04-02 23:14:14 +0700

    implement search box header

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,12 @@
+import styles from './Sidebar.module.scss'
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h2>Sidebar
-    
-    </h2>
+    return <aside className={cx('wrapper')}>
+
+    </aside>
 }
 
 export default Sidebar;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,32 @@
 import styles from './Header.module.scss';
 import classNames from "classnames/bind";
+import images from "~/assets/images";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleXmark, faMagnifyingGlass, faSpinner} from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return <h2 className={cx('wrapper')}>
         <div className={cx('inner')}>
-        {/*    Logo */}
-        {/*    Search */}
+            <div className={cx('logo')}>
+                <img src={images.logo} alt='Tiktok'/>
+            </div>
+            <div className={cx('search')}>
+                <input placeholder={'Search accounts and video'} spellCheck={false}/>
+                <button className={cx('clear')}>
+                    <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                </button>
+                <FontAwesomeIcon className={cx('loading')} icon={faSpinner}></FontAwesomeIcon>
+                {/*    Loading */}
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+                    {/*    Search button */}
+                </button>
+            </div>
+            <div className={cx('action')}>
+                {/*    Action */}
+            </div>
         </div>
     </h2>;
 }
