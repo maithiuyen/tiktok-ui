0a3bed9abd920b417886fd0f8422c32762ca120d
ChungNV <nguyenvanchung281296@gmail.com>
2023-04-16 11:22:14 +0700

    implement menu item in header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,13 @@
 import classNames from "classnames/bind";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCircleXmark, faMagnifyingGlass, faSignIn, faSpinner} from "@fortawesome/free-solid-svg-icons";
+import {
+    faCircleQuestion,
+    faCircleXmark, faEarthAsia,
+    faEllipsisVertical, faKeyboard,
+    faMagnifyingGlass,
+    faSignIn,
+    faSpinner
+} from "@fortawesome/free-solid-svg-icons";
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 import {useEffect, useState} from "react"; // optional
@@ -10,8 +17,25 @@ import images from "~/assets/images";
 import styles from './Header.module.scss';
 import {Wrapper as PopperWrapper} from "~/components/Popper";
 import AccountItem from "~/components/AccountItem";
+import Menu from "~/components/Popper/Menu";
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia}/>,
+        title: 'English'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion}/>,
+        title: 'Feedback and help',
+        to: '/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard}/>,
+        title: 'Keyboard shortcuts'
+    },
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -33,18 +57,19 @@ function Header() {
                 interactive
                 visible={searchResult.length > 0}
                 render={(attrs) => (
-                        <div className={cx('search-result')} tabIndex='-1' {...attrs}>
-                            <PopperWrapper>
-                                <h4 className={cx('search-title')}>
-                                    Accounts
-                                </h4>
-                                <AccountItem/>
-                                <AccountItem/>
-                                <AccountItem/>
-                                <AccountItem/>
-                            </PopperWrapper>
-                        </div>
-                )}>
+                    <div className={cx('search-result')} tabIndex='-1' {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>
+                                Accounts
+                            </h4>
+                            <AccountItem/>
+                            <AccountItem/>
+                            <AccountItem/>
+                            <AccountItem/>
+                        </PopperWrapper>
+                    </div>
+                )}
+            >
                 <div className={cx('search')}>
                     <input placeholder={'Search accounts and video'} spellCheck={false}/>
                     <button className={cx('clear')}>
@@ -59,10 +84,16 @@ function Header() {
 
             </Tippy>
             <div className={cx('action')}>
-                <Button text >Upload</Button>
-                <Button primary  >
+                <Button text>Upload</Button>
+                <Button primary>
                     Log in
                 </Button>
+
+                <Menu items={MENU_ITEMS}>
+                    <button className={cx('more-btn')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical}/>
+                    </button>
+                </Menu>
             </div>
         </div>
     </h2>;
